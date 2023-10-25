c983a9855a1a94fb02aeaa5ead2f76a32e30418d
Andrés Yesid Moreno Villa <65937165+Mastercol01@users.noreply.github.com>
2023-10-25 18:01:50 -0500

    Add files via upload

--- serial_thermal.py
+++ serial_thermal.py
@@ -2,7 +2,7 @@ import os
 import serial
 from datetime import datetime
 
-filename = 'serial_thermal_data.txt'
+filename = 'serial_thermal_data_v1_20231025_1630.txt'
 filepath = os.path.join(os.getcwd(), filename)
 
 if __name__ == '__main__':
@@ -14,6 +14,9 @@ if __name__ == '__main__':
         while True:
             if ser.in_waiting > 0:
                 line = ser.readline().decode('utf-8').rstrip()
+                line = line.replace('CLEARDATA,', '')
+                line = line.replace('CLEARSHEET,', '')
+                line = line.replace('RESETTIMER,', '')
                 line = line.replace('LABEL, ', '')
                 line = line.replace('Date, Time', 'DateTime')
                 line = line.replace('DATA,', '')
