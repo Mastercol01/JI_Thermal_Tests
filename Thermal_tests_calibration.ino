c6bd1537d5b2bb0cb538c7ffd2a1ede0ff3131be
Andrés Yesid Moreno Villa <65937165+Mastercol01@users.noreply.github.com>
2023-10-25 18:03:22 -0500

    Add files via upload

---
+++ Thermal_tests_calibration.ino
@@ -0,0 +1,214 @@
+// -------------- IMPORTATION OF LIBRARIES --------------
+#include <stdio.h>     
+#include <math.h>       
+#include "DHT.h"
+#include <SPI.h>
+#include <mcp2515.h>
+
+
+// (1)-------------- GENERAL SET-UP --------------(1)
+uint32_t initialTime;
+uint32_t sendDataTimer;
+uint32_t sendDataTimeout = 1000;
+
+bool canDataRelevant = false;
+bool dhtDataRelevant = false;
+bool RtDataRelevant  = true;
+
+
+
+// (2)-------------- DHT SET-UP AND RELATED FUNCS --------------(2)
+#define DHTPIN 3
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+const int NUM_THdht_AVGS = 10;
+
+float Tdht;
+float Hdht;
+
+void readUpdateTHdht(){
+  Tdht = 0;
+  Hdht = 0;
+
+  for(int i=0; i<NUM_THdht_AVGS; i++){
+    Tdht += dht.readTemperature();
+    Hdht += dht.readHumidity();
+    }
+  
+  Tdht /= NUM_THdht_AVGS;
+  Hdht /= NUM_THdht_AVGS;
+}
+
+
+
+
+// (3)-------------- THERMISTOR SET-UP AND RELATED FUNCS --------------(3)
+
+const int NUM_RT_PINS          = 5;
+const int RT_PINS[NUM_RT_PINS] = {A2, A3, A4, A5, A6};
+float     R1[NUM_RT_PINS]      = {32970.0, 32460.0, 33350.0, 32990.0, 32540.0};
+const int NUM_Tt_AVGS          = 30;
+
+float Vcc = 5.012;
+float Rt[NUM_RT_PINS];
+float Vt[NUM_RT_PINS];
+float Tt[NUM_RT_PINS];
+
+
+void readUpdateVt(){
+  for(int i=0; i<NUM_RT_PINS; i++){
+    Vt[i] = 0;
+    for(int j=0; j<NUM_Tt_AVGS; j++){
+      Vt[i] += analogRead(RT_PINS[i]);
+      }
+      Vt[i] /= NUM_Tt_AVGS;
+      Vt[i] = Vcc*Vt[i]/1023.0;
+  }
+}
+
+void updateRt(){
+  for(int i=0; i<NUM_RT_PINS; i++){
+    Rt[i] = R1[i]*Vt[i]/(Vcc - Vt[i]);
+  }
+}
+
+void updateTt(){
+  float lnRt;
+  float A =  610.5282190830485;
+  float B = -85.97402403405586;
+  float C =  3.4037509247172553;
+  float D = -0.007982876975644648;
+  float E = -0.0019710091731159445;
+
+  for(int i=0; i<NUM_RT_PINS; i++){
+  lnRt = log(Rt[i]);
+  Tt[i] = A + B*lnRt + C*pow(lnRt,2) + D*pow(lnRt,3) + E*pow(lnRt,4);
+  }
+}
+
+
+
+// (4)-------------- CAN SET-UP --------------(4)
+struct can_frame canMsg;
+MCP2515 mcp2515(10);
+
+float batteryVoltage = NAN;
+float batteryCurrent = NAN;
+float batterySOC = NAN;
+
+void readUpdateBatteryData(){
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    if(canMsg.can_id == 2550245121){
+      batteryVoltage = 0.1*(  (canMsg.data[1] << 8) | canMsg.data[0]);
+      batteryCurrent = 0.1*( ((canMsg.data[3] << 8) | canMsg.data[2]) - 32000);
+      batterySOC     = 1.0*canMsg.data[4];
+    }
+  }
+}
+
+
+
+
+// (5)-------------- PRINT DATA --------------(5)
+
+void excelSetup(){
+
+  String setupString = "LABEL, Date, Time, Milliseconds";
+
+  if(canDataRelevant){
+    setupString = setupString + ",V,I,Soc";} 
+
+  if(dhtDataRelevant){
+    setupString = setupString + ",Tdht,Hdht";}
+
+  if(RtDataRelevant){
+    for(int i=0; i<NUM_RT_PINS; i++){setupString = setupString + ",Vt" + i;}
+    for(int i=0; i<NUM_RT_PINS; i++){setupString = setupString + ",Rt" + i;}
+    for(int i=0; i<NUM_RT_PINS; i++){setupString = setupString + ",Tt" + i;}    
+  }
+
+  Serial.println("CLEARDATA");
+  Serial.println("CLEARSHEET");
+  Serial.println(setupString);
+  Serial.println("RESETTIMER");
+}
+
+void sendData(){
+  String dataString = "DATA,DATE,TIME";
+  dataString = dataString + "," + (millis() - initialTime);
+
+  if(canDataRelevant){
+    dataString = dataString + "," + batteryVoltage;
+    dataString = dataString + "," + batteryCurrent;
+    dataString = dataString + "," + batterySOC;
+  }
+
+  if(dhtDataRelevant){
+    dataString = dataString + "," + Tdht; 
+    dataString = dataString + "," + Hdht; 
+  }
+
+  if(RtDataRelevant){
+    for(int i=0; i<NUM_RT_PINS; i++){dataString = dataString + "," + Vt[i];}
+    for(int i=0; i<NUM_RT_PINS; i++){dataString = dataString + "," + Rt[i];}
+    for(int i=0; i<NUM_RT_PINS; i++){dataString = dataString + "," + Tt[i];}
+  }
+
+  Serial.println(dataString);
+
+}
+
+
+
+
+void setup() {
+  // SET THERMISTOR PINS TO INPUT.
+  
+  for(int i=0; i<NUM_RT_PINS; i++){
+    pinMode(RT_PINS[i], INPUT);
+  }
+
+  // INITIALIZE SERIAL AND DHT SENSOR.
+  Serial.begin(9600);
+  while (!Serial){;}
+  dht.begin();
+
+  // INITIALIZE CAN MODULE
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_250KBPS, MCP_8MHZ);
+  mcp2515.setNormalMode();
+
+
+  // INITIALIZE TIME VARIABLES.
+  initialTime   = millis();
+  sendDataTimer = millis();
+
+  // EXECUTE EXCEL SET-UP.
+  excelSetup();
+
+}
+
+
+
+void loop() {
+
+  // BATTERY CAN DATA
+  readUpdateBatteryData();
+  
+  // DHT SENSOR
+  readUpdateTHdht();
+
+  // THERMISTORS
+  readUpdateVt();
+  updateRt();
+  updateTt();
+
+
+  // SEND DATA OVER TO EXCEL
+  if(millis() - sendDataTimer > sendDataTimeout){
+    sendData();
+    sendDataTimer = millis();
+    }
+
+    
+  }
